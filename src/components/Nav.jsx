e395828810dc2b96d3db84bd60d323a0f470c0f1
Rafly <muhr0417@gmail.com>
2025-06-24 23:16:53 +0700

    make first navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+const Nav = () => {
+  return (
+    <nav className="sticky top-0 py-2">
+      <div className="flex-center md:justify-between">
+        <div className="hidden md:inline p-5">
+          <h1>Raf</h1>
+        </div>
+        <div className="flex p-5 gap-10">
+          <a href="" className="Clickable px-2">Home</a>
+          <a href="">Project</a>
+          <a href="">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
