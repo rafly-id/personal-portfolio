284b0f029ebfd9d4726613bd6855cd943f65d107
Rafly <muhr0417@gmail.com>
2025-06-24 23:18:57 +0700

    make first home

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <section>
+      <div className="h-screen">
+        <div className="mt-25 max-w-2xl justify-start mx-5 md:ml-15 md:text-3xl font-light md:mt-50">
+          <p className="mb-5">
+            Raf Portofolio adalah untuk menampilkan karya dan proses kreatif
+            saya.
+          </p>
+          <p className="mb-5">
+            Bukan untuk memamerkan segalanya atau sekadar daftar prestasi—ia
+            hadir agar setiap ide dan solusi tersaji dengan jernih, bebas
+            gangguan, bak lembar kertas digital yang siap diisi.
+          </p>
+          <p>Raf Portofolio adalah untuk menampilkan apa saja.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
