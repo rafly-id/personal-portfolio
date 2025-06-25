60df128536faed97346efd0d4e695cda79ec00c3
Rafly <muhr0417@gmail.com>
2025-06-26 02:04:24 +0700

    change some sentence

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,29 @@ const Home = () => {
   return (
     <section>
       <div className="h-screen">
-        <div className="mt-25 max-w-2xl justify-start mx-5 md:ml-15 md:text-3xl font-light md:mt-50">
-          <p className="mb-5">
-            Raf Portofolio adalah untuk menampilkan karya dan proses kreatif
-            saya.
-          </p>
-          <p className="mb-5">
-            Bukan untuk memamerkan segalanya atau sekadar daftar prestasi—ia
-            hadir agar setiap ide dan solusi tersaji dengan jernih, bebas
-            gangguan, bak lembar kertas digital yang siap diisi.
-          </p>
-          <p>Raf Portofolio adalah untuk menampilkan apa saja.</p>
+        <div className="mt-25 mb-10 max-w-2xl justify-start mx-5 tracking-tight md:ml-15 md:text-3xl md:mt-50">
+          <div>
+            <p className="mb-5">
+              <span>Raf portfolio.</span> A simple place where I organize my
+              work, share my progress, and record my journey.
+            </p>
+            <p className="mb-5">
+              It's not about showing off, but about sharing stories of ideas,
+              mistakes, and small meaningful steps.
+            </p>
+            <p className="mb-5">
+              Here, I show how I learn, create, and keep moving forward—not
+              perfect, but genuine.
+            </p>
+            <p className="mb-5">
+              Maybe you'll find something here, maybe you won't. There's more to
+              come, and I'm excited to keep going.
+            </p>
+            <p>This is me and my journey.</p>
+          </div>
+          {/* <div className="flex justify-start mt-5 md:mt-10 py-5 gap-5 md:gap-15k">
+            <button className="Clickable px-10 py-3 text-xl hover:rounded-xl">Button</button>
+          </div> */}
         </div>
       </div>
     </section>
