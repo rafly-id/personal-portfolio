7f7f6b88ed1a3477e6337fe1156fadc623d3c770
Rafly <muhr0417@gmail.com>
2025-06-27 23:33:37 +0700

    update smoother for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 // App.jsx
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 
-const App = () => (
-  <>
-    <Nav />
-    <Home />
-  </>
-);
+// register plugin sekali
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const App = () => {
+  useEffect(() => {
+    const smoother = ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+      wrapper: "#wrapper-smooth",
+      content: "#content-smooth",
+    });
+
+    ScrollTrigger.refresh();
+
+    return () => {
+      ScrollTrigger.killAll();
+      smoother.kill();
+    };
+  }, []);
+
+  return (
+    <>
+      <Nav />
+      <section id="wrapper-smooth">
+        <div id="content-smooth">
+          <Home />
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,6 @@ import { ScrollTrigger, ScrollSmoother } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const Home = () => {
-  useEffect(() => {
-    const smoother = ScrollSmoother.create({
-      smooth: 3,
-      wrapper: "#wrapper-smooth",
-      content: "#content-smooth",
-    });
-
-    ScrollTrigger.refresh();
-
-    return () => {
-      ScrollTrigger.getAll()
-        .filter((t) => t.trigger === document.querySelector(".text-container"))
-        .forEach((t) => t.kill());
-      smoother.kill();
-    };
-  }, []);
-
   useGSAP(() => {
     gsap.from(".text", {
       opacity: 0,
@@ -43,43 +26,39 @@ const Home = () => {
   });
 
   return (
-    <section id="wrapper-smooth">
-      <div id="content-smooth">
-        <div className="mt-50 mb-10 max-w-2xl justify-start mx-5 tracking-tight md:ml-15 md:text-3xl md:mt-80">
-          <div className="text-container">
-            <p className="mb-3 md:mb-5">
-              Raf portofolio.
-              <span className="text"> A simple place where I</span>
-              <span className="text"> organize my work,</span>
-              <span className="text"> share my progress,</span>
-              <span className="text"> and record my journey.</span>
-            </p>
-            <p className="mb-3 md:mb-5">
-              <span className="text">
-                It's not about showing off, but about sharing
-              </span>
-              <span className="text"> stories of ideas,</span>
-              <span className="text"> mistakes,</span>
-              <span className="text"> and small meaningful steps.</span>
-            </p>
-            <p className="mb-3 md:mb-5">
-              <span className="text">Here, I show how I</span>
-              <span className="text"> learn,</span>
-              <span className="text"> create,</span>
-              <span className="text"> and keep moving forward—</span>
-              <span className="text">not perfect, but genuine.</span>
-            </p>
-            <p className="mb-3 md:mb-5 text">
-              Maybe you'll find something here, maybe you won't. There's more to
-              come, and I'm excited to keep going.
-            </p>
-            <p className="text">This is me and my journey.</p>
-          </div>
+    <>
+      <div className="mt-50 mb-10 max-w-2xl justify-start mx-5 tracking-tight md:ml-15 md:text-3xl md:mt-80">
+        <div className="text-container">
+          <p className="mb-3 md:mb-5">
+            Raf portofolio.
+            <span className="text"> A simple place where I</span>
+            <span className="text"> organize my work,</span>
+            <span className="text"> share my progress,</span>
+            <span className="text"> and record my journey.</span>
+          </p>
+          <p className="mb-3 md:mb-5">
+            <span className="text">
+              It's not about showing off, but about sharing
+            </span>
+            <span className="text"> stories of ideas,</span>
+            <span className="text"> mistakes,</span>
+            <span className="text"> and small meaningful steps.</span>
+          </p>
+          <p className="mb-3 md:mb-5">
+            <span className="text">Here, I show how I</span>
+            <span className="text"> learn,</span>
+            <span className="text"> create,</span>
+            <span className="text"> and keep moving forward—</span>
+            <span className="text">not perfect, but genuine.</span>
+          </p>
+          <p className="mb-3 md:mb-5 text">
+            Maybe you'll find something here, maybe you won't. There's more to
+            come, and I'm excited to keep going.
+          </p>
+          <p className="text">This is me and my journey.</p>
         </div>
-        <div className="h-screen"></div>
-        <div className="h-screen"></div>
       </div>
-    </section>
+    </>
   );
 };
 
