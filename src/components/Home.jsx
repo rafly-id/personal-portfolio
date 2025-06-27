34e90b3b81e253390b5905fc005e38505db0f5d1
Rafly <muhr0417@gmail.com>
2025-06-27 21:16:47 +0700

    little update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -47,14 +47,14 @@ const Home = () => {
       <div id="content-smooth">
         <div className="mt-50 mb-10 max-w-2xl justify-start mx-5 tracking-tight md:ml-15 md:text-3xl md:mt-80">
           <div className="text-container">
-            <p className="mb-5">
-              Raf portofolio.{" "}
-              <span className="text">A simple place where I</span>
+            <p className="mb-3 md:mb-5">
+              Raf portofolio.
+              <span className="text"> A simple place where I</span>
               <span className="text"> organize my work,</span>
               <span className="text"> share my progress,</span>
               <span className="text"> and record my journey.</span>
             </p>
-            <p className="mb-5">
+            <p className="mb-3 md:mb-5">
               <span className="text">
                 It's not about showing off, but about sharing
               </span>
@@ -62,14 +62,14 @@ const Home = () => {
               <span className="text"> mistakes,</span>
               <span className="text"> and small meaningful steps.</span>
             </p>
-            <p className="mb-5">
+            <p className="mb-3 md:mb-5">
               <span className="text">Here, I show how I</span>
               <span className="text"> learn,</span>
               <span className="text"> create,</span>
               <span className="text"> and keep moving forward—</span>
               <span className="text">not perfect, but genuine.</span>
             </p>
-            <p className="mb-5 text">
+            <p className="mb-3 md:mb-5 text">
               Maybe you'll find something here, maybe you won't. There's more to
               come, and I'm excited to keep going.
             </p>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,11 @@
 const Nav = () => {
   return (
-    <nav className="nav-fixed py-2 bg-black/90 md:bg-transparent">
-      <div className="flex-center md:justify-between">
-        <div className="hidden md:inline p-5">
+    <nav className="nav-fixed py-5 bg-black/50 md:bg-transparent">
+      <div className="flex-center justify-between px-5 md:px-0">
+        <div className="md:p-5">
           <h1>Raf</h1>
         </div>
-        <div className="flex p-5 gap-10">
+        <div className="flex md:p-5 gap-3 md:gap-10">
           <a href="#" className="Clickable px-2">
             Home
           </a>
