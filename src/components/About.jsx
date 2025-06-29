f41c519cd2a397e408e6192a600af159cafe9324
Rafly <muhr0417@gmail.com>
2025-06-30 03:31:49 +0700

    change desain , i can't desain btw

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
         scroller: "#wrapper-smooth",
         trigger: containerRef.current,
         start: "top center",
-        end: "150px center",
+        end: "250px center",
         scrub: 1,
       },
     });
@@ -30,39 +30,33 @@ const About = () => {
 
   return (
     <section
-      className="pb-25 md:pb-50 mx-5 tracking-tight text-xl md:text-3xl"
+      className="pb-[100px] md:pb-[250px] md:mx-20 mx-5 -tracking-wider md:-tracking-widest"
       ref={containerRef}
     >
-      <div className="max-w-2xl md:ml-15">
-        <div className="mb-5 text-3xl font-oswald">
-          <h1>RAF PORTOFOLIO</h1>
+      <div className="text-center text-xl md:text-5xl font-black uppercase">
+        <div className="mb-5 font-light text-xs md:text-sm tracking-widest">
+          <h1>ABOUT</h1>
         </div>
-        <p className="mb-3 md:mb-5">
-          <span className="text"> A simple space where I</span>
-          <span className="text"> organize my work,</span>
-          <span className="text"> share my progress,</span>
-          <span className="text"> and record my journey.</span>
-        </p>
-        <p className="mb-3 md:mb-5">
-          <span className="text">
-            It's not about showing off, but about sharing
-          </span>
-          <span className="text"> stories of ideas,</span>
-          <span className="text"> mistakes,</span>
-          <span className="text"> and small meaningful steps.</span>
-        </p>
-        <p className="mb-3 md:mb-5">
-          <span className="text">Here, I show how I</span>
-          <span className="text"> learn,</span>
-          <span className="text"> create,</span>
-          <span className="text"> and keep moving forward—</span>
-          <span className="text">not perfect, but genuine.</span>
-        </p>
-        <p className="mb-3 md:mb-5 text">
+        <span className="text"> A simple space where I</span>
+        <span className="text"> organize my work,</span>
+        <span className="text"> share my progress,</span>
+        <span className="text"> and record my journey. </span>
+        <span className="text">
+          It's not about showing off, but about sharing
+        </span>
+        <span className="text"> stories of ideas,</span>
+        <span className="text"> mistakes,</span>
+        <span className="text"> and small meaningful steps.</span>
+        <span className="text"> Here, I show how I</span>
+        <span className="text"> learn,</span>
+        <span className="text"> create,</span>
+        <span className="text"> and keep moving forward—</span>
+        <span className="text"> not perfect, but genuine. </span>
+        <span className="text">
           Maybe you'll find something here, maybe you won't. There's more to
           come, and I'm excited to keep going.
-        </p>
-        <p className="text">This is me and my journey.</p>
+        </span>
+        <span className="text">This is me and my journey.</span>
       </div>
     </section>
   );
