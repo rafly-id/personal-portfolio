8083949527102318951f96a37e54b2a49b5c0353
Rafly <muhr0417@gmail.com>
2025-06-30 03:45:30 +0700

    change desain home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,13 @@
 const Home = () => {
   return (
-    <section className="md:min-h-screen mx-20 mb-50 md:mb-0">
-      <div>
-        <div className="flex justify-center items-center mt-30 mb-20 md:mb-30">
-          <img
-            src="./src/assets/profile.jpeg"
-            alt="profile"
-            className="w-xs rounded-xl"
-          />
-        </div>
+    <section className="mb-52 md:mb-80">
+      <div className="flex justify-end items-end mb-5">
+        <img src="./src/assets/profile.jpeg" alt="profile" className="w-xl" />
       </div>
-      <div>
-        <div className="flex items-center justify-center -tracking-widest text-4xl md:text-9xl w-full font-oswald">
-          <h1>RAFLY ADRIANSYAH</h1>
-        </div>
+      <div className="flex text-center justify-center">
+        <h1 className="-tracking-wider text-3xl md:text-7xl max-w-full font-oswald uppercase">
+          Hey. I'm Muhammad Rafly Adriansyah - a junior frontend developer from indonesia
+        </h1>
       </div>
     </section>
   );
